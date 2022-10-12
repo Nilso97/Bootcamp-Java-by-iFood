636848fdd12fa240b7be389351622ce9aebd546f
Nilso Junior <96146165+Nilso97@users.noreply.github.com>
2022-10-12 12:15:00 -0300

    Exercícios Bootcamp Java by iFood

--- src/Desafios_parte1/src/Desafios/Desafio.java
+++ src/Desafios_parte1/src/Desafios/Desafio.java
@@ -1,29 +1,30 @@
-// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
-// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
-// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.
+package Desafios;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Desafio {
-
+    
     public static void main(String[] args) {
-
-        //TODO: Imprima se os valores numéricos passados são iguais ou não.
         
-        Scanner leitor = new Scanner(System.in);
-		
-		    // System.out.print("Informe o primeiro valor: ");
-		    int a = leitor.nextInt();
-		
-		    // System.out.print("Informe o segundo valor: ");
-		    int b = leitor.nextInt();
-		
-		    if (a == b) {
-		       System.out.println("\nSao iguais!");
-		    } else {
-			     System.out.println("\nNao sao iguais!");
-		    }
-		
-		    leitor.close();
+        Scanner input = new Scanner(System.in);
+
+        int a, n;
+		int soma = 0;
+
+        // System.out.print("Infome o primeiro valor: ");
+        a = input.nextInt();
+
+        // System.out.print("Infome o valor final: ");
+        n = input.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            if (i % a == 0) {
+                soma += i;
+            }
+        }
+
+        System.out.println(soma);
+        
+        input.close();
     }
 }
